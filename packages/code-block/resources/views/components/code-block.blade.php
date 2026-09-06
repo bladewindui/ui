4a39c7241fa99df444e007f236c87c5adfcffd96
Michael K. Ocansey <kabutey@gmail.com>
2026-09-06 22:16:01 +0000

    Reduce code block vertical padding further to 4px

--- packages/code-block/resources/views/components/code-block.blade.php
+++ packages/code-block/resources/views/components/code-block.blade.php
@@ -88,7 +88,7 @@
 
     <pre @if($highlightLines !== '') data-line="{{ $highlightLines }}" @endif
          @class([
-            "language-$language !m-0 !rounded-none !py-2",
+            "language-$language !m-0 !rounded-none !py-1",
             'line-numbers' => $lineNumbers,
             '!whitespace-pre-wrap break-words' => $wrap,
          ])><code data-bw-code-block-source class="language-{{ $language }}">{{ $code }}</code></pre>
